f7f36c88eb165f03ceacf0c668e9cfd5266a0655
Vincent Philippe <vincentcharles.philippe@gmail.com>
2024-11-19 17:27:01 +0100

    new references

--- app/components/nodes/classifications/classifications.tsx
+++ app/components/nodes/classifications/classifications.tsx
@@ -3,71 +3,104 @@ import Figure from "@/app/components/figure/figure.tsx";
 import Hypothesis from "@/app/components/hypothesis/hypothesis.tsx";
 import { MyNode } from "@/models/node";
 import bibtex from "@/public/citations/bibtex.json";
+import NodeCard from "../../node-card/node-card";
 
 const Classifications = new MyNode(
-  "Classifications",
-  [],
-  (
-    <div>
-      <CitationCard citations={[bibtex.knopman2018national]}>
-        individuals without overt symptoms, called “preclinical AD” [4].
-      </CitationCard>
-      <CitationCard citations={[bibtex.knopman2018national]}>
-        Biomarkers of AD: CSF biomarkers are valid proxies for neuropathologic
-        changes of AD. Imaging-to-autopsy comparison studies have established
-        that amyloid positron emission tomography (PET) is a valid in vivo
-        surrogate for Ab deposits (in brain parenchyma/vessel walls)
-        [17–24].Itis also now widely accepted that CSF Ab42 (or the Ab42/ Ab40
-        ratio) is a valid indicator of the abnormal pathologic state associated
-        with cerebral Ab [25]. An additional development has been the
-        introduction of PET ligands for pathologic tau [26–28]. magnetic
-        resonance imaging (MRI), fluorodeoxyglucose (FDG) PET, and CSF total tau
-        (T-tau)—are not specific for AD but rather are nonspecific indicators of
-        damage that may derive from a variety of etiologies, for example,
-        cerebrovascular injury [29].
-      </CitationCard>
-      <CitationCard citations={[bibtex.knopman2018national]}>
-        <Figure
-          src="images/classifcation/classification.png"
-          alt="Classification problem"
-          caption="Interaction of ATN features"
-          n={1}
-        />
-        <Figure
-          src="images/classifcation/atn_niaaa.png"
-          alt="ATN X NIA-AA"
-          caption="Biomarkers interactions with NIA-AA"
-          n={2}
-        />
-      </CitationCard>
-      <CitationCard citations={[bibtex.knopman2018national]}>
-        Comparision NIA-AA with IWG: The International Working Group (IWG) also
-        incorporates biomarkers in AD diagnosis but requires cognitive symptoms
-        alongside biomarkers for diagnosis (except for genetic AD). Unlike
-        NIA-AA’s purely biological model, IWG initially required clinical
-        symptoms to define AD. However, IWG’s 2016 update included asymptomatic
-        individuals with both abnormal Aβ and tau biomarkers in the AD category,
-        aligning more closely with NIA-AA’s biologic definition.
-      </CitationCard>
-      <CitationCard citations={[bibtex.knopman2018national]}>
-        Alzheimer’s clinical syndrome rather than probable AD when describing
-        syndromes without biomarker evidence
-      </CitationCard>
-      <Hypothesis>
-        <Figure
-          src="images/causality/causal_hypothesis.png"
-          alt="Hypothesis"
-          caption="The possible causalities of the biomarkers"
-          n={1}
-        />
-      </Hypothesis>
-      <CitationCard citations={[bibtex.arevalo2015mini]}>
-        he prevalence of cognitive impairment without dementia as 22% in people
-        aged 71 years or older (Plassman 2008).10% to 15% of individuals with
-        MCI may progress to dementia,
-      </CitationCard>
-    </div>
-  )
+    "Classifications",
+    [],
+    (
+        <NodeCard>
+            <CitationCard citations={[bibtex.sperling2011toward]}>
+                individuals without overt symptoms, called “preclinical AD”.
+            </CitationCard>
+            <span>
+                <CitationCard
+                    citations={[
+                        bibtex.mormino2014synergistic,
+                        bibtex.jack2017age,
+                        bibtex.jack2017defining,
+                        bibtex.jagust2009relationships,
+                        bibtex.landau2013comparing,
+                        bibtex.leuzy2016pittsburgh,
+                        bibtex.palmqvist2014accuracy,
+                        bibtex.zwan2014concordance,
+                    ]}
+                >
+                    Biomarkers of AD: CSF biomarkers are valid proxies for
+                    neuropathologic changes of AD. Imaging-to-autopsy comparison
+                    studies have established that amyloid positron emission
+                    tomography (PET) is a valid in vivo surrogate for Ab
+                    deposits (in brain parenchyma/vessel walls).
+                </CitationCard>
+                <CitationCard citations={[bibtex.ossenkoppele2012longitudinal]}>
+                    Itis also now widely accepted that CSF Aß42 (or the Ab42/
+                    Aß40 ratio) is a valid indicator of the abnormal pathologic
+                    state associated with cerebral Aß.
+                </CitationCard>
+                <CitationCard
+                    citations={[
+                        bibtex.clark2012cerebral,
+                        bibtex.ikonomovic2016post,
+                        bibtex.murray2015clinicopathologic,
+                    ]}
+                >
+                    An additional development has been the introduction of PET
+                    ligands for pathologic tau.
+                </CitationCard>
+                <CitationCard citations={[bibtex.chhatwal2016temporal]}>
+                    MRI, FDG-PET, and CSF total tau (T-tau)—are not specific for
+                    AD but rather are nonspecific indicators of damage that may
+                    derive from a variety of etiologies, for example,
+                    cerebrovascular injury.
+                </CitationCard>
+            </span>
+            <CitationCard citations={[bibtex.knopman2018national]}>
+                <Figure
+                    src="images/classifcation/classification.png"
+                    alt="Classification problem"
+                    caption="Interaction of ATN features"
+                    n={1}
+                />
+                <Figure
+                    src="images/classifcation/atn_niaaa.png"
+                    alt="ATN X NIA-AA"
+                    caption="Biomarkers interactions with NIA-AA"
+                    n={2}
+                />
+            </CitationCard>
+            <CitationCard citations={[bibtex.knopman2018national]}>
+                Comparision NIA-AA with IWG: The International Working Group
+                (IWG) also incorporates biomarkers in AD diagnosis but requires
+                cognitive symptoms alongside biomarkers for diagnosis (except
+                for genetic AD). Unlike NIA-AA’s purely biological model, IWG
+                initially required clinical symptoms to define AD. However,
+                IWG’s 2016 update included asymptomatic individuals with both
+                abnormal Aβ and tau biomarkers in the AD category, aligning more
+                closely with NIA-AA’s biologic definition.
+            </CitationCard>
+            <CitationCard citations={[bibtex.knopman2018national]}>
+                Alzheimer’s clinical syndrome rather than probable AD when
+                describing syndromes without biomarker evidence
+            </CitationCard>
+            <Hypothesis>
+                <Figure
+                    src="images/causality/causal_hypothesis.png"
+                    alt="Hypothesis"
+                    caption="The possible causalities of the biomarkers"
+                    n={1}
+                />
+            </Hypothesis>
+            <span>
+                <CitationCard citations={[bibtex.plassman2008prevalence]}>
+                    he prevalence of cognitive impairment without dementia as
+                    22% in people aged 71 years or older.
+                </CitationCard>
+                <CitationCard citations={[bibtex.arevalo2015mini]}>
+                    10% to 15% of individuals with MCI may progress to dementia,
+                </CitationCard>
+            </span>
+        </NodeCard>
+    )
 );
 
 export const Wrapper = () => <>{Classifications.content}</>;
--- app/components/nodes/classifications/mmse/mmse.tsx
+++ app/components/nodes/classifications/mmse/mmse.tsx
@@ -3,39 +3,70 @@ import { MyNode } from "@/models/node";
 import bibtex from "@/public/citations/bibtex.json";
 
 const MMSE = new MyNode(
-  "Mini Mental State Examination (MMSE)",
-  [],
-  (
-    <div>
-      The MMSE is subject to copyright restrictions, therefore will not be
-      included in this resource.
-      <CitationCard citations={[bibtex.arevalo2015mini]}>
-        MMSE is the best-known and the most oRen used short screening tool for
-        providing an overall measure of cognitive impairment in clinical,
-        research and community settings. sensitivities of 23% to 76% and
-        specificities from 40% to 94%. n relationship to conversion from MCI to
-        Alzheimer’s disease dementia, the accuracy of baseline MMSE scores
-        ranged from sensitivities of 27% to 89% and specificities from 32% to
-        90%. In conclusion it should not be used as a standalone diagnostic.
-      </CitationCard>
-      <CitationCard citations={[bibtex.arevalo2015mini]}>
-        a 30-question assessment of cognitive function that evaluates attention
-        and orientation, memory, registration, recall, calculation, language and
-        ability to draw a complex polygon ( Folstein 1975). Traditionally, a
-        23/24 cut-off has been used to select patients with suspected cognitive
-        impairment or dementia ( Tombaugh 1992 ). However, several studies have
-        shown that sociocultural variables, age and education, among other
-        factors, could affect individual scores (Bleecker 1988; Brayne 1990;
-        Crum 1993); therefore local standards must be developed for each
-        population and setting evaluated (Diniz 2007; Kulisevsky 2009; Shiroky
-        2007; Trenkle 2007).
-      </CitationCard>
-      <CitationCard citations={[bibtex.arevalo2015mini]}>
-        Alternatives: Montreal Cognitive Assessment (MoCA) (Davis 2013) and the
-        Mini-Cog test (Chan 2014, Fage 2013, Seitz 2014).
-      </CitationCard>
-    </div>
-  )
+    "Mini Mental State Examination (MMSE)",
+    [],
+    (
+        <div>
+            The MMSE is subject to copyright restrictions, therefore will not be
+            included in this resource.
+            <CitationCard citations={[bibtex.arevalo2015mini]}>
+                MMSE is the best-known and the most oRen used short screening
+                tool for providing an overall measure of cognitive impairment in
+                clinical, research and community settings. sensitivities of 23%
+                to 76% and specificities from 40% to 94%. n relationship to
+                conversion from MCI to Alzheimer’s disease dementia, the
+                accuracy of baseline MMSE scores ranged from sensitivities of
+                27% to 89% and specificities from 32% to 90%. In conclusion it
+                should not be used as a standalone diagnostic.
+            </CitationCard>
+            <span>
+                <CitationCard citations={[bibtex.folstein1975mini]}>
+                    a 30-question assessment of cognitive function that
+                    evaluates attention and orientation, memory, registration,
+                    recall, calculation, language and ability to draw a complex
+                    polygon.
+                </CitationCard>
+                <CitationCard citations={[bibtex.tombaugh1992mini]}>
+                    Traditionally, a 23/24 cut-off has been used to select
+                    patients with suspected cognitive impairment or dementia.
+                </CitationCard>
+                <CitationCard
+                    citations={[
+                        bibtex.bleecker1988age,
+                        bibtex.brayne1990association,
+                        bibtex.crum1993population,
+                    ]}
+                >
+                    However, several studies have shown that sociocultural
+                    variables, age and education, among other factors, could
+                    affect individual scores
+                </CitationCard>
+                <CitationCard
+                    citations={[
+                        bibtex.diniz2007mini,
+                        bibtex.kulisevsky2009cognitive,
+                        bibtex.shiroky2007can,
+                        bibtex.trenkle2007detecting,
+                    ]}
+                >
+                    therefore local standards must be developed for each
+                    population and setting evaluated.
+                </CitationCard>
+                <CitationCard citations={[bibtex.davis2013neuropsychological]}>
+                    Alternatives: Montreal Cognitive Assessment (MoCA)
+                </CitationCard>
+                <CitationCard
+                    citations={[
+                        bibtex.chan2019mini,
+                        bibtex.fage2015mini,
+                        bibtex.seitz2014mini,
+                    ]}
+                >
+                    and the Mini-Cog test.
+                </CitationCard>
+            </span>
+        </div>
+    )
 );
 
 export const Wrapper = () => <>{MMSE.content}</>;
